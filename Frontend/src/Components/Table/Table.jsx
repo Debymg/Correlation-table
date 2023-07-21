fe7f70f6511aa818d55e1b205a6b227e3e89e1bf
Deby <deborah.murati@gmail.com>
2023-07-21 14:11:25 -0400

    add python app

--- Frontend/src/Components/Table/Table.jsx
+++ Frontend/src/Components/Table/Table.jsx
@@ -18,14 +18,16 @@ const Table = () => {
     <table className="correlation-grid">
     <thead>
       <tr>
+      <th></th> {/* Agregamos una celda vacía para alinear correctamente el encabezado con las columnas */}
         {variables.map((variable, index) => (
-          <th key={index}>{variable}</th>
+        <th key={index}>{variable}</th>
         ))}
       </tr>
     </thead>
     <tbody>
       {variables.map((_, rowIndex) => (
         <tr key={rowIndex}>
+          <th>{variable}</th> {/* Agregamos el nombre de la variable como primera celda de cada fila */}
           {variables.map((_, columnIndex) => (
             <td key={columnIndex}>Value</td> // Puedes reemplazar "Value" con los valores reales de correlación.
           ))}
